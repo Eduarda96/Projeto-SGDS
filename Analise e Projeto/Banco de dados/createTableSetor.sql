7fe4e21991e58a13b3a56d2710fd2f7894ec7f43
Rodrigo Antônio Menezes Maria <rammaria@restinga.ifrs.edu.br>
2015-10-15 16:49:36 -0300

    Atualizado createTableSetor
    Atualizado por Rodrigo e Aschley

--- Analise e Projeto/Banco de dados/createTableSetor.sql
+++ Analise e Projeto/Banco de dados/createTableSetor.sql
@@ -1,9 +1,12 @@
+CREATE database sgds_banco;
+
 CREATE table SETOR(
-	id int unsigned NOT NULL auto_increment,
+	codSetor int unsigned NOT NULL auto_increment,
 	nome varchar (50) NOT NULL,
 	nomeResponsavel varchar (100) NOT NULL,
-	setorResponsavel varchar (100) NOT NULL,
+	setorResponsavel int unsigned,
 	descricao text NOT NULL,
 	email varchar (50) NOT NULL,
-	primary key(ID)
+	ativo boolean,
+	primary key(codSetor)
 );
