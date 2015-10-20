8fe5f5172df04820fc499cd806a37e7473cd332e
vrnery <vrnery@restinga.ifrs.edu.br>
2015-10-20 15:40:50 -0200

    Ajustes do usuario do banco.

--- Analise e Projeto/Banco de dados/createTableSetor.sql
+++ Analise e Projeto/Banco de dados/createTableSetor.sql
@@ -1,5 +1,11 @@
 CREATE database sgds_banco;
 
+USE sgds_banco;
+
+CREATE USER 'usersgds'@'localhost' IDENTIFIED BY 'bundamole';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE, SHOW VIEW ON sgds_banco.* TO 'usersgds'@'localhost';
+
 CREATE table SETOR(
 	codSetor int unsigned NOT NULL auto_increment,
 	nome varchar (50) NOT NULL,
